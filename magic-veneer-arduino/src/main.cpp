2cb72a73e8e6b70c0df6c757f6b0a4e7ef85ed32
Dennis Schoepf <dennis@schoepf.co>
2020-09-17 11:34:41 +0200

    Initial commit

---
+++ magic-veneer-arduino/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#include <SharpIR.h>
+
+SharpIR sensor(SharpIR::GP2Y0A02YK0F, A0);
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
+  // put your setup code here, to run once:
+}
+
+void loop()
+{
+  int distance = sensor.getDistance();
+  Serial.println(distance);
+}
